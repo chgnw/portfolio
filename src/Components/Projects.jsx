929047da29b250a0b968cec7111340056f6426a9
chgnw <christophergunawan10@gmail.com>
2024-10-12 13:44:01 +0700

    Adding 1 New Project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,7 @@
 import proj1 from '../img/expenseTracker-EndProduct.png'
 import proj2 from '../img/ToDoList-EndProduct.png'
 import proj3 from '../img/KeyboardDrum-EndProduct.png'
+import proj4 from '../img/Calculator.png'
 
 const Project = () => {
     return (
@@ -64,6 +65,25 @@ const Project = () => {
                             <div className="mt-4 flex justify-end">
                                 <button className="bg-acc-l px-2 py-1 rounded-lg text-white hover:bg-acc-d dark:bg-acc-d dark:hover:bg-acc-l">
                                     <a href="https://github.com/chgnw/KeyboardDrumGames.git">View in GitHub</a>
+                                </button>
+                            </div>
+                        </div>                       
+                    </div>
+
+                    <div id='proj-4' className='flex flex-col rounded-lg border-2 border-acc-l hover:border-acc-d'>
+                        <div className="">
+                            <img src={proj4} alt="" className="rounded-t-lg"/>
+                        </div>
+
+                        <div className="p-2">
+                            <h5 className="font-semibold text-md md:text-lg lg:text-2xl">Calculator</h5>
+                            <p className="font-medium text-justify text-sm md:text-md lg:text-lg">
+                                Basic calculator function that build using React framework and TailwindCSS for the styling.
+                            </p>
+
+                            <div className="mt-4 flex justify-end">
+                                <button className="bg-acc-l px-2 py-1 rounded-lg text-white hover:bg-acc-d dark:bg-acc-d dark:hover:bg-acc-l">
+                                    <a href="https://github.com/chgnw/Calculator.git">View in GitHub</a>
                                 </button>
                             </div>
                         </div>                       
