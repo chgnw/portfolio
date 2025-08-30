ce4937adbdc8c0a52bc9186180679121402561f3
chgnw <christophergunawan10@gmail.com>
2025-08-30 16:18:05 +0700

    save creds

--- src/config/emailjs.ts
+++ src/config/emailjs.ts
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-    SERVICE_ID: 'YOUR_SERVICE_ID', // Replace with your EmailJS service ID
-    TEMPLATE_ID: 'YOUR_TEMPLATE_ID', // Replace with your EmailJS template ID
-    PUBLIC_KEY: 'YOUR_PUBLIC_KEY', // Replace with your EmailJS public key
+    SERVICE_ID: 'YOUR_SERVICE_ID', 
+    TEMPLATE_ID: 'YOUR_TEMPLATE_ID',
+    PUBLIC_KEY: 'YOUR_PUBLIC_KEY', 
 }; 
